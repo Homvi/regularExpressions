4f32bf5e14f89bfe79b26dfa32b52d739a7c4c8f
AdrianMontes <adrian.montes01@estudiant.upf.edu>
2023-11-30 13:52:11 +0100

    Added more endpoints

--- backend/backend/src/main/java/com/example/controllers/HomeController.java
+++ backend/backend/src/main/java/com/example/controllers/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.pojos.Expression;
 import com.example.pojos.User;
 
 @Controller
@@ -18,6 +19,7 @@ public class HomeController {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
+	//Test
 	@ResponseBody
 	@RequestMapping(value = "/users")
 	public List<User> GetUsers() {
@@ -40,9 +42,19 @@ public class HomeController {
 	@ResponseBody
 	@RequestMapping(value = "/register")
 	public void AddUser(User user) {
-		String query = "INSERT INTO DB_REGEXPRESSIONS.users (firstName, surname, username, email, password, isAdmin) VALUES"
-				+ "'" + user.getFirstName() + "', '" + user.getSurname() + "', '" + user.getEmail() + "', '" + user.getPassword() + "', " +user.isAdmin() + "')";
+		String query = "INSERT INTO DB_REGEXPRESSIONS.users (firstName, surname, username, email, password, isAdmin) VALUES (?, ?, ?, ?, ?, ?)";
 		String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(20));
-		jdbcTemplate.update(query, user.getFirstName());
+		jdbcTemplate.update(query, user.getFirstName(), user.getSurname(), user.getSurname(), user.getUsername(), user.getEmail(), hashedPassword, 0);
+	}
+	
+	public static boolean CheckPassword(String password, String hashedPassword) {
+		return BCrypt.checkpw(password, hashedPassword);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/requestExpression")
+	public void AddExpression(Expression expression) {
+		String query = "INSERT INTO DB_REGEXPRESSIONS.expressions (languageOfExpression, expression, rightAnswer, falseAnswerOne, falseAnswerTwo, creatorId, validated) VALUES (?, ?, ?, ?, ?, ?, ?);";
+		jdbcTemplate.update(query, expression.getLanguage(), expression.getExpression(), expression.getRightAnswer(), expression.getWrongAnswerOne(), expression.getWrongAnswerTwo(), expression.getCreatorId(), expression.isValidated());
 	}
 }
