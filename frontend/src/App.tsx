1c999973e3412722541417e7cd5c744f2f9223f9
Homvi <honvedoadamkj@gmail.com>
2023-12-01 10:52:33 +0100

    add login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Users from "./pages/Users";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/users" element={<Users />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,76 @@
+import axios from "axios";
+import { FormEvent, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const user = {
+      email: email,
+      password: password,
+    };
+
+    console.log(user);
+    try {
+      console.log("Trying to post data");
+      const response = await axios.post("http://localhost:8080/register", user);
+      console.log(response);
+      toast("You have succesfully logged in!");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      toast("Oops it didn't work!");
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center">
+      <ToastContainer />
+      <h1 className="text-center text-3xl my-3">Register</h1>
+      {/* form */}
+      <form
+        onSubmit={handleSubmit}
+        className="w-full md:w-[70%] border-[1px] border-gray-100 p-6 flex flex-col mx-auto my-3"
+      >
+        {/* email input */}
+        <label className="form-control w-full">
+          <div className="label">
+            <span className="label-text">Email</span>
+          </div>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            required
+            placeholder="email"
+            className="input input-bordered w-full"
+          />
+        </label>
+        {/* password input */}
+        <label className="form-control w-full">
+          <div className="label">
+            <span className="label-text">Password</span>
+          </div>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            required
+            className="input input-bordered w-full"
+          />
+        </label>
+        <button type="submit" className="btn btn-primary my-3">
+          Log in
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
