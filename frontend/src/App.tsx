9362a1088fec9a281bd5b8a8b845f1fa969dce21
Homvi <honvedoadamkj@gmail.com>
2023-12-01 10:58:52 +0100

    add root route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import "./App.css";
 import Users from "./pages/Users";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/users" element={<Users />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
