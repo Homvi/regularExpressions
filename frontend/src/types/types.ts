36609c61a060ba9b7b4ec181cbbdedd956abadc9
Homvi <honvedoadamkj@gmail.com>
2023-12-05 11:02:38 +0100

    add expressions type

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,11 @@
+type ExpressionType = {
+  id: number;
+  expression: string;
+  rightAnswer: string;
+  falseAnswerOne: string;
+  falseAnswerTwo: string;
+};
+
+// Path: frontend/src/types/types.ts
+
+export type { ExpressionType };
