0a2aa3c22e6bd51ca7388f19c77ae831b547255e
Homvi <honvedoadamkj@gmail.com>
2023-12-05 11:48:18 +0100

    randomise answers

--- frontend/src/components/Expression.tsx
+++ frontend/src/components/Expression.tsx
@@ -16,6 +16,21 @@ const Expression: React.FC<ExpressionProps> = ({
     setLoaded(true);
   }, []);
 
+  //create function that gives me 3 random numbers between 0 and 2
+  const getRandomNumbers = () => {
+    const randomNumbers:number[] = [];
+    while (randomNumbers.length < 3) {
+      const randomNumber = Math.floor(Math.random() * 3);
+      if (!randomNumbers.includes(randomNumber)) {
+        randomNumbers.push(randomNumber);
+      }
+    }
+    return randomNumbers;
+  };
+
+  //use those numbers to randomize the order of the answers
+  const randomNumbers = getRandomNumbers();
+
   return (
     <div
       className={
@@ -26,18 +41,21 @@ const Expression: React.FC<ExpressionProps> = ({
     >
       <h2 className="text-2xl">{expression.expression}</h2>
       <div
+        style={{ order: randomNumbers[0] }}
         onClick={() => handleChoice(expression.rightAnswer)}
-        className="bg-blue-200 min-w-[200px] min-h-[150px] md:min-w-[500px] flex justify-center items-center p-3 rounded-lg cursor-pointer"
+        className="bg-green-200 min-w-[200px] min-h-[150px] md:min-w-[500px] flex justify-center items-center p-3 rounded-lg cursor-pointer"
       >
         <p>{expression.rightAnswer}</p>
       </div>
       <div
+        style={{ order: randomNumbers[1] }}
         className="bg-blue-200 min-w-[200px] min-h-[150px] md:min-w-[500px] flex justify-center items-center p-3 rounded-lg cursor-pointer"
         onClick={() => handleChoice(expression.falseAnswerOne)}
       >
         <p>{expression.falseAnswerOne}</p>
       </div>
       <div
+        style={{ order: randomNumbers[2] }}
         className="bg-blue-200 min-w-[200px] min-h-[150px] md:min-w-[500px] flex justify-center items-center p-3 rounded-lg cursor-pointer"
         onClick={() => handleChoice(expression.falseAnswerTwo)}
       >
