cbc61d6f24f28c752bbdf96533810522ad2c1607
Homvi <honvedoadamkj@gmail.com>
2023-12-05 09:38:54 +0100

    return after logout to home page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import { UserContext } from "../UserContext";
 import { useContext } from "react";
+import { redirect } from "react-router-dom";
 
 const Navbar = () => {
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
   const handleLogout = () => {
     setLoggedInUser({});
+    return redirect("/");
   };
 
   return (
