c0bcef3d14dfe2795d321eb8d54726736ce1d5fd
Homvi <honvedoadamkj@gmail.com>
2023-12-05 12:57:22 +0100

    add play again button

--- frontend/src/components/Score.tsx
+++ frontend/src/components/Score.tsx
@@ -1,12 +1,15 @@
 const Score = ({ score, resetGame }) => {
   return (
     <div className="bg-black/25 absolute left-0 right-0 bottom-0 top-0 flex justify-center items-center">
-      <div className="bg-white min-h-[300px] min-w-[300px]">
+      <div className="bg-white/90 min-h-[300px] min-w-[300px] rounded-xl">
         <div onClick={resetGame} className="p-3 cursor-pointer">
           X
         </div>
-        <div className="flex h-full justify-center items-center">
+        <div className="flex h-full justify-center items-center flex-col gap-6">
           <h1>Your score is: {(score / 10) * 100}%</h1>
+          <button onClick={resetGame} className="btn btn-primary">
+            Play again
+          </button>
         </div>
       </div>
     </div>
