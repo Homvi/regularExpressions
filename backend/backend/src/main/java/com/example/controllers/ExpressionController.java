d25e1eb9f01ef750378bf00f3f4c8a33960fbe96
Alvaro Carceles <albotarell@gmail.com>
2023-12-07 09:04:09 +0100

    Added breadkpoint to upload expressions

--- backend/backend/src/main/java/com/example/controllers/ExpressionController.java
+++ backend/backend/src/main/java/com/example/controllers/ExpressionController.java
@@ -2,11 +2,16 @@ package com.example.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entities.Expression;
+import com.example.entities.User;
 import com.example.services.ExpressionService;
 
 @RestController
@@ -26,5 +31,15 @@ public class ExpressionController {
 	@GetMapping("/getEnglishExpressions")
 	public List<Expression> getEnglishExpressions() {
 		return expressionService.getExpressions("english");
+	}
+	
+	@PostMapping("/myExpressions")
+    public ResponseEntity<?> addExpression(@RequestBody Expression expression) {
+        try {
+            expressionService.addExpression(expression);
+            return new ResponseEntity<>("Expresión agregada exitosamente", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error al agregar la expresión", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 	}
 }
--- backend/backend/src/main/java/com/example/services/ExpressionService.java
+++ backend/backend/src/main/java/com/example/services/ExpressionService.java
@@ -2,12 +2,18 @@ package com.example.services;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.entities.Expression;
+import com.example.entities.User;
 import com.example.repository.ExpressionRepository;
 
 @Service
@@ -28,4 +34,8 @@ public class ExpressionService {
 	    System.out.println(expressions);
 	    return expressions.stream().limit(10).collect(Collectors.toList());
 	}
+	
+    public void addExpression(Expression expression) {
+        expressionRepository.save(expression);
+    }
 }
