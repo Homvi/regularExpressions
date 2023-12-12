cd3a879aa69048322c0b74eeb5e42c3dd183f68a
AdrianMontes <adrian.montes01@estudiant.upf.edu>
2023-12-12 13:40:59 +0100

    Added an endpoint to send the favorite expressions

--- backend/backend/src/main/java/com/example/controllers/FavoritesController.java
+++ backend/backend/src/main/java/com/example/controllers/FavoritesController.java
@@ -3,10 +3,13 @@ package com.example.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entities.Expression;
+import com.example.entities.User;
 import com.example.services.FavoritesService;
 
 @RestController
@@ -18,11 +21,22 @@ public class FavoritesController {
 		this.favoritesService = favoritesService;
 	}
 	
+	@PostMapping("/addFavorite")
 	public ResponseEntity<?> addFavorite(@RequestBody Expression expression) {
 		try {
+			favoritesService.addFavorite(expression);
             return new ResponseEntity<>("Marked as favorite", HttpStatus.OK);
 		} catch (Exception e) {
-            return new ResponseEntity<>("There has been an error when marking as favorite", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("There has been an error when marking as favorite", HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@GetMapping("/getFavorites")
+	public ResponseEntity<?> getFavorites(@RequestBody User loginUser) {
+		try {
+			return favoritesService.getFavorites(loginUser.getId());
+		} catch (Exception e) {
+            return new ResponseEntity<>("There has been an error getting the favorites", HttpStatus.NOT_FOUND);
 		}
 	}
 }
--- backend/backend/src/main/java/com/example/services/FavoritesService.java
+++ backend/backend/src/main/java/com/example/services/FavoritesService.java
@@ -1,16 +1,23 @@
 package com.example.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.entities.Expression;
 import com.example.entities.Favorite;
+import com.example.repository.ExpressionRepository;
 import com.example.repository.FavoritesRepository;
 
 @Service
 public class FavoritesService {
 	@Autowired
 	private FavoritesRepository favoritesRepository;
+	@Autowired
+	private ExpressionRepository expressionRepository;
 	
 	FavoritesService(FavoritesRepository favoritesRepository) {
 		this.favoritesRepository = favoritesRepository;
@@ -21,5 +28,21 @@ public class FavoritesService {
 		favorite.setUserId(expression.getCreatorId());
 		favorite.setExpressionId(expression.getId());
 		favoritesRepository.save(favorite);
+	}
+	
+	public ResponseEntity<?> getFavorites(Long userId) {
+		List<Favorite> favoriteExpressionsId = favoritesRepository.findAll().stream()
+	            .filter(expression -> userId.equals(expression.getId()))
+	            .collect(Collectors.toList());
+		List<Long> expressionIds = favoriteExpressionsId.stream()
+		        .map(Favorite::getExpressionId)
+		        .collect(Collectors.toList());
+
+		List<Expression> userExpressions = expressionRepository.findAll()
+		        .stream()
+		        .filter(expression -> expressionIds.contains(expression.getId()))
+		        .collect(Collectors.toList());
+		
+		return ResponseEntity.ok(userExpressions);
 	}
 }
