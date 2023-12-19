9aa982d9f0d6249cc469bc9130dcca8d2e424d60
Homvi <honvedoadamkj@gmail.com>
2023-12-19 13:54:24 +0100

    Merge branch 'develop' of https://github.com/Homvi/regularExpressions into develop

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -164,7 +164,7 @@ const Game: React.FC<GameExpressionsProps> = ({ isFontSizeLarge }) => {
           <ProgressBar progress={progress} />
           <div
             id="expressionsContainer"
-            className={`flex flex-col text-center justify-center w-full md:max-w-xl items-center gap-3 transition-all duration-300${
+            className={`flex flex-col text-center justify-center w-full md:max-w-xl items-center gap-3 transition-all duration-300 ${
               fadeIn
                 ? "opacity-0 translate-x-full"
                 : "opacity-100 translate-x-0"
